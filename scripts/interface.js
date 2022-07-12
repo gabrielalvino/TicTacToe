4e432c901aa07dd261858ba0f05e9c831ee62d57
gabrielalvino <gabrielalvino2008@gmail.com>
2022-07-11 21:35:34 -0300

    done tic tac toe

---
+++ scripts/interface.js
@@ -0,0 +1,38 @@
+var squares = document.querySelectorAll(".squares");
+
+document.addEventListener("DOMContentLoaded", ()=> {
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+    })
+})
+
+function handleClick(event){
+
+
+	let square = event.target;
+	let position = square.id;
+
+	if (handleMove(position)){
+
+    setTimeout(() => {
+      alert ("You win, the game is over!!" + `the winner is player '${playerTime +  1}`)
+    }, 10);
+
+  }
+
+    updateSquares(position);
+}
+
+
+
+
+
+function updateSquares(position){
+
+    let square = document.getElementById(position.toString());
+		let symbol = board[position];
+		square.innerHTML = `<div class='${symbol}'</div>`
+}
+
+
